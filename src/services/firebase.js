2c06fee0746d68a6bdac49b69f26d5d442011516
HoangAnhNguyen0710 <anhnh1@hblab.vn>
2023-02-15 16:19:05 +0700

    update Search User function

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -29,6 +29,19 @@ export async function getUserByUsername(username) {
   }));
 }
 
+export async function getAllOtherUsers(userId) {
+  const result = await firebase
+  .firestore()
+  .collection("users")
+  .where("userId", "!=", userId)
+  .get();
+
+return result.docs.map((item) => ({
+  ...item.data(),
+  docId: item.id,
+}));
+}
+
 // get user from the firestore where userId === userId (passed from the auth)
 export async function getUserByUserId(userId) {
   const result = await firebase
